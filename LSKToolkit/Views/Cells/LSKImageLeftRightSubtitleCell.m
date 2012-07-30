4c74cb867b2304db7f1660be29ec2da456561171
Luosky <luosky@gmail.com>
2012-07-30 16:30:15 +0800

    fixed error

--- LSKToolkit/Views/Cells/LSKImageLeftRightSubtitleCell.m
+++ LSKToolkit/Views/Cells/LSKImageLeftRightSubtitleCell.m
@@ -8,16 +8,17 @@
 
 #import "LSKImageLeftRightSubtitleCell.h"
 #import "UIColor+Expanded.h"
+#import "LSKConstants.h"
 @implementation LSKImageLeftRightSubtitleCell
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
     if ((self = [super initWithStyle:style reuseIdentifier:reuseIdentifier])) {
         self.cellHeight = 60;
         
-        self.imageButton.frame = CGRectMake(10, (self.contentView.height - 24) / 2 , 24, 24);
+        self.imageButton.frame = CGRectMake(10, (CGRectGetHeight(self.contentView.frame) - 24) / 2 , 24, 24);
         
-        int maxHeight = self.contentView.height - 2 * CELL_OUTTER_MARGIN;
-        self.subtitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.contentView.width - 40 - 60, CELL_OUTTER_MARGIN, 60, maxHeight)];
+        int maxHeight = CGRectGetHeight(self.contentView.frame)  - 2 * CELL_OUTTER_MARGIN;
+        self.subtitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(CGRectGetWidth(self.contentView.frame)  - 40 - 60, CELL_OUTTER_MARGIN, 60, maxHeight)];
         self.subtitleLabel.backgroundColor = [UIColor clearColor];
         self.subtitleLabel.textColor = [UIColor colorWithHexString:@"86898E"];
         self.subtitleLabel.font = [UIFont fontWithName:@"Arial-BoldMT" size:12];
@@ -27,7 +28,7 @@
         [self.contentView addSubview:self.subtitleLabel];
         
         int titleX = CELL_OUTTER_MARGIN + CGRectGetMaxX(self.imageButton.frame);
-        self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(titleX, CELL_OUTTER_MARGIN, self.contentView.width - titleX - 40 - self.subtitleLabel.width, maxHeight)];
+        self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(titleX, CELL_OUTTER_MARGIN, CGRectGetWidth(self.contentView.frame)  - titleX - 40 - CGRectGetWidth(self.subtitleLabel.frame), maxHeight)];
         self.titleLabel.backgroundColor = [UIColor clearColor];
         self.titleLabel.font = [UIFont fontWithName:@"Arial-BoldMT" size:17];
         self.titleLabel.textColor = [UIColor colorWithHexString:@"351F18"];
