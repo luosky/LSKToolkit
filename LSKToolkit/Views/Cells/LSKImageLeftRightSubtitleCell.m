2212c4a9f9afc1f6c9d09462bd61d1418b2afac3
Luosky <luosky@gmail.com>
2012-07-30 17:01:26 +0800

    right label align to right. adjusted the width .

--- LSKToolkit/Views/Cells/LSKImageLeftRightSubtitleCell.m
+++ LSKToolkit/Views/Cells/LSKImageLeftRightSubtitleCell.m
@@ -18,19 +18,20 @@
         self.imageButton.frame = CGRectMake(10, (CGRectGetHeight(self.contentView.frame) - 24) / 2 , 24, 24);
         
         int maxHeight = CGRectGetHeight(self.contentView.frame)  - 2 * CELL_OUTTER_MARGIN;
-        self.subtitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(CGRectGetWidth(self.contentView.frame)  - 40 - 60, CELL_OUTTER_MARGIN, 60, maxHeight)];
+        self.subtitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(CGRectGetWidth(self.contentView.frame)  - 40 - 60 - 5, CELL_OUTTER_MARGIN, 60, maxHeight)];
         self.subtitleLabel.backgroundColor = [UIColor clearColor];
         self.subtitleLabel.textColor = [UIColor colorWithHexString:@"86898E"];
-        self.subtitleLabel.font = [UIFont fontWithName:@"Arial-BoldMT" size:12];
+        self.subtitleLabel.textAlignment = UITextAlignmentRight;
+        //        self.subtitleLabel.font = [UIFont fontWithName:@"Arial-BoldMT" size:12];
         self.subtitleLabel.adjustsFontSizeToFitWidth = YES;
         self.subtitleLabel.autoresizingMask = UIViewAutoresizingFlexibleHeight;
         self.subtitleLabel.minimumFontSize = 10.0f;
         [self.contentView addSubview:self.subtitleLabel];
         
         int titleX = CELL_OUTTER_MARGIN + CGRectGetMaxX(self.imageButton.frame);
-        self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(titleX, CELL_OUTTER_MARGIN, CGRectGetWidth(self.contentView.frame)  - titleX - 40 - CGRectGetWidth(self.subtitleLabel.frame), maxHeight)];
+        self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(titleX, CELL_OUTTER_MARGIN, CGRectGetWidth(self.contentView.frame)  - titleX - 40 -5 - CGRectGetWidth(self.subtitleLabel.frame), maxHeight)];
         self.titleLabel.backgroundColor = [UIColor clearColor];
-        self.titleLabel.font = [UIFont fontWithName:@"Arial-BoldMT" size:17];
+        //        self.titleLabel.font = [UIFont fontWithName:@"Arial-BoldMT" size:17];
         self.titleLabel.textColor = [UIColor colorWithHexString:@"351F18"];
         self.titleLabel.autoresizingMask = UIViewAutoresizingFlexibleHeight;
         self.titleLabel.adjustsFontSizeToFitWidth = YES;
